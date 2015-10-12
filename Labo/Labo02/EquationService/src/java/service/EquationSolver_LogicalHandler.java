291aca72bd43f647cac0e79df7626437e1062217
vdbbjorn <bjorn.vandenbussche@ugent.be>
2015-10-12 13:04:45 +0200

    logicalhandler

---
+++ Labo/Labo02/EquationService/src/java/service/EquationSolver_LogicalHandler.java
@@ -0,0 +1,61 @@
+package service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.LogicalMessage;
+import javax.xml.ws.handler.LogicalHandler;
+import javax.xml.ws.handler.LogicalMessageContext;
+import javax.xml.ws.handler.MessageContext;
+import org.w3c.dom.Document;
+
+/**
+ *
+ * @author Bjorn
+ */
+public class EquationSolver_LogicalHandler implements LogicalHandler<LogicalMessageContext> {
+    
+    @Override
+    public boolean handleMessage(LogicalMessageContext messageContext) {
+        Boolean outbound = (Boolean) messageContext.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+        if (!outbound) {
+            try {
+                LogicalMessage msg = messageContext.getMessage();
+                Source inhoud = msg.getPayload();
+                TransformerFactory factory = TransformerFactory.newInstance();
+                Transformer tf = factory.newTransformer(new StreamSource(this.getClass().getClassLoader().getResourceAsStream("ws/nieuweVersie.xsl")));
+                DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = dFactory.newDocumentBuilder();
+                Document document = builder.newDocument();
+                DOMResult aangepast = new DOMResult(document);
+                tf.transform(inhoud, aangepast);
+                msg.setPayload(new DOMSource(aangepast.getNode()));
+            } catch (TransformerConfigurationException | ParserConfigurationException ex) {
+                Logger.getLogger(EquationSolver_LogicalHandler.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (TransformerException ex) {
+                Logger.getLogger(EquationSolver_LogicalHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean handleFault(LogicalMessageContext messageContext) {
+        return true;
+    }
+    
+    @Override
+    public void close(MessageContext context) {
+    }
+    
+}
