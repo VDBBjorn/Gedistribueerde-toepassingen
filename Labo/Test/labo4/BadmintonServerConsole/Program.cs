01d814b0033d8d9ac5bc22b65ff85f450a84fa59
vdbbjorn <bjorn.vandenbussche@ugent.be>
2015-11-23 13:56:27 +0100

    server over tcp

---
+++ Labo/Test/labo4/BadmintonServerConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadmintonServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(BadmintonServiceLibrary.BadmintonService)))
+            {
+                host.Open();
+                Console.WriteLine("De BadmintonServer is klaar.");
+                Console.WriteLine("Klik <ENTER> om af te sluiten.");
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
